032f13574a2f8ec1437ec6d80fd298988600a42e
LUCASMSALDANA <lucasmsaldana@gmail.com>
2025-10-25 20:59:12 -0300

    Primer ejercicio. Super básico mostrar por pantalla

---
+++ src/Seccion2/Presentate.java
@@ -0,0 +1,14 @@
+package Seccion2;
+
+// Ejercicio:
+// Crear una nueva clase llamada Presentate.java en la  cual debes hacer lo siguiente:
+// Agregar el metodo Main
+// Mostrar en la consola: Nombre, edad y país.
+
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Lucas Martin Saldaña");
+        System.out.println("32");
+        System.out.println("Argentina");
+    }
+}
