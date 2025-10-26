648c5f7e722f13a0045a9faa6a120be1aca15b8f
LUCASMSALDANA <lucasmsaldana@gmail.com>
2025-10-26 16:44:45 -0300

    Seccion 5, 47. Ejemplo - Sistemas de Empelados

---
+++ src/seccion5/SistemaEmpleados.java
@@ -0,0 +1,48 @@
+package seccion5;
+// Se solicita crear un programa para pedir la información de un empleado.
+// Los datos a introducir por consola son:
+// Nombre del empleado.
+// Edad (int)
+// Salario (double)
+// Es jefe de departamento (boolean)
+// Luego mostrar por pantalla
+// Usar nextLine y convertir segun corresponda
+
+// Nota personal, falta el manejo de errores... asume que el usuario es perfecto...
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class SistemaEmpleados {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*** Sistema de Empleados ***");
+        // Comienzo a pedir datos
+
+        //Nombre del empleado
+        System.out.print("Nombre del empleado: ");
+        String nombreEmpelado = scanner.nextLine();
+
+        //Edad del empleado
+        System.out.print("Edad del empleado: ");
+        int edad = Integer.parseInt(scanner.nextLine());
+
+        //Salario del empleado
+        System.out.print("Salario del empleado: ");
+        double salario = Double.parseDouble(scanner.nextLine());
+
+        //Es jefe?
+        System.out.print("Jefe de departamento: ");
+        boolean esJefe = Boolean.parseBoolean(scanner.nextLine());
+
+        System.out.println("\nDatos del empleado\n");
+
+        System.out.println("Nombre: " +nombreEmpelado);
+        System.out.println("Edad: " + edad);
+        // System.out.println("Salario: " + salario);  Esta opción es válida pero vamos a ver como se formatea
+        System.out.println("Salario: $%.2f".formatted(salario)); // De esta manera indico que voy a formatearlo .
+        System.out.println("Es jefe de departamento: " + esJefe);
+
+    }
+}
