f188a2ffda2d000b7772b6e0e8e8af4afc528aac
LUCASMSALDANA <lucasmsaldana@gmail.com>
2025-11-06 15:03:51 -0300

    Seccion 5, Aplicamos lo aprendido en el video 52

--- src/seccion5/FormateoDeCadenas.java
+++ src/seccion5/FormateoDeCadenas.java
@@ -14,5 +14,27 @@ public class FormateoDeCadenas {
         //System.out.printf()
         System.out.println("\nUtilizando printf");
         System.out.printf("Nombre: %s Edad: %d Salario: %.2f %n",nombre,edad,salario); //usando format el %n hace un salto de linea
+
+        int idEmpleado=1;
+        //Formateo con text block """
+        mensaje= """
+                %nDetalle de la persona:
+                -----------------------
+                \tNombre: %s
+                \tID empleado: %04d
+                \tEdad: %d años
+                \tSalario: $%.2f
+                """.formatted(nombre,idEmpleado,edad,salario);
+        System.out.println(mensaje);
+
+        //Formateo con printf
+        System.out.printf("""
+                %nDetalle de la persona:
+                -----------------------
+                \tNombre: %s
+                \tID empleado: %04d
+                \tEdad: %d años
+                \tSalario: $%.2f
+                """,nombre,idEmpleado,edad,salario);
     }
 }
