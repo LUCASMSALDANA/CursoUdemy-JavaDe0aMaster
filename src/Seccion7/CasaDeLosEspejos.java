bff09e3b161476edf7462ce8d52aaab6dabe4dfa
LUCASMSALDANA <lucasmsaldana@gmail.com>
2025-11-10 15:13:26 -0300

    Seguimos practicando segun lo visto en la sección 7. En este caso practicamos. La logica inversa

---
+++ src/Seccion7/CasaDeLosEspejos.java
@@ -0,0 +1,21 @@
+package Seccion7;
+
+import java.util.Scanner;
+
+public class CasaDeLosEspejos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*** Bienvenido a la casa de los Espejos ***");
+
+        System.out.print("Que edad tienes? :");
+        int edad = Integer.parseInt(scanner.nextLine());
+        System.out.print("Le tienes miedo a la oscuridad? (true/false): ");
+        boolean tieneMiedoAlaOscuridad = Boolean.parseBoolean(scanner.nextLine());
+
+        if(!tieneMiedoAlaOscuridad && edad>=10){
+            System.out.println("Puedes entrar a la casa de los espejos");
+        }else{
+            System.out.println("La casa de los espejos podria ser espeluznante para ti");
+        }
+    }
+}
