3eae36691f9f33d614c143e1e007c58d995f374f
LUCASMSALDANA <lucasmsaldana@gmail.com>
2025-11-13 19:57:50 -0300

    Seguimos practicando ciclos for, while y switch

---
+++ src/Seccion8/RetoCicloForNumerosPares.java
@@ -0,0 +1,15 @@
+package Seccion8;
+
+//Utilizar el ciclo for para imprimir numeros pares
+
+public class RetoCicloForNumerosPares {
+    public static void main(String[] args) {
+
+        for (int i = 0; i<=20; i++){
+            if (i%2 ==0){
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
---
+++ src/Seccion8/SistemaAdministracionDeCuentas.java
@@ -0,0 +1,59 @@
+package Seccion8;
+
+import java.util.Scanner;
+
+public class SistemaAdministracionDeCuentas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Con la notación clasica del Switch
+//        boolean salir = false;
+//
+//        while (!salir){
+//            System.out.print("""
+//                    *** Sistema de Administración de Cuentas ***
+//                    1. Crear cuenta.
+//                    2. Eliminar cuenta.
+//                    3. Salir
+//                    Escoja una opción :\s""");
+//            int opcion = Integer.parseInt(scanner.nextLine());
+//            switch (opcion){
+//                case 1:
+//                    System.out.println("Creando cuenta...\n");
+//                    break;
+//                case 2:
+//                System.out.println("Eliminando cuenta...\n");
+//                    break;
+//                case 3:
+//                    System.out.println("Saliendo del sistema...\n");
+//                    salir=true;
+//                    break;
+//                default:
+//                    System.out.println("Opción incorrecta.");
+//                    break;
+//            }
+//        }
+        // Con la notacion mejorada del switch:
+        boolean salir = false;
+
+        while (!salir) {
+            System.out.print("""
+                    *** Sistema de Administración de Cuentas ***
+                    1. Crear cuenta.
+                    2. Eliminar cuenta.
+                    3. Salir
+                    Escoja una opción :\s""");
+            int opcion = Integer.parseInt(scanner.nextLine());
+            switch (opcion) {
+                case 1 -> System.out.println("Creando cuenta...\n");
+                case 2 -> System.out.println("Eliminando cuenta...\n");
+                case 3 -> {
+                    System.out.println("Saliendo del sistema...\n");
+                    salir = true;
+                }
+                default -> System.out.println("Opción incorrecta.");
+
+            }
+        }
+    }
+}
