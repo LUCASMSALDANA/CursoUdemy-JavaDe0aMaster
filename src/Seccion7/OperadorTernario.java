f52b72959d5fba754861c969767db2ad159dbb2e
LUCASMSALDANA <lucasmsaldana@gmail.com>
2025-11-13 12:36:50 -0300

    Comenzamos a ver la seccion 8 con ciclos, además teniamos un ejemplo sin rellenar sobre los operadoresternarios

--- src/Seccion7/OperadorTernario.java
+++ src/Seccion7/OperadorTernario.java
@@ -1,7 +1,15 @@
 package Seccion7;
 
+import java.util.Scanner;
+
 public class OperadorTernario {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Ingrese su edad: ");
+        int edad = Integer.parseInt(scanner.nextLine());
+
+        String mensaje= (edad>=18) ? "Puede ingresar, es mayor de edad" : "No puede ingresar, es menor";
+        System.out.println(mensaje);
     }
 }
